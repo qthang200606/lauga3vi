9b0328379b932f7065d414be045410961ffc7377
qthang200606 <thang93dhp@gmail.com>
2026-09-11 10:17:50 +0700

    fix

--- server/models/ChatMessage.js
+++ server/models/ChatMessage.js
@@ -1,14 +1,12 @@
 const mongoose = require("mongoose");
 
-const chatMessageSchema = new mongoose.Schema(
-  {
-    roomId: { type: String, required: true, index: true },
-    sender: { type: String, enum: ["client", "admin"], required: true },
-    senderName: { type: String, default: "Khách vãng lai" },
-    message: { type: String, required: true },
-    time: { type: String },
-  },
-  { timestamps: true }
-);
+const chatMessageSchema = new mongoose.Schema({
+  roomId: { type: String, required: true },
+  sender: { type: String, required: true },
+  senderName: { type: String, default: "Khách vãng lai" },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" }, // <--- THÊM DÒNG NÀY
+  message: { type: String, required: true },
+  time: { type: String }
+}, { timestamps: true });
 
 module.exports = mongoose.model("ChatMessage", chatMessageSchema);
